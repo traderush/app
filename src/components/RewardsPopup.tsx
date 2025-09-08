a173ba09136d92a44d346d3fe035ae89015397db
Eugene Romanov <romanovyev@gmail.com>
2025-09-08 09:48:47 -0400

    Make SVG chart more compact: reduce viewBox width from 300 to 240, adjust line end point from 220 to 210

--- src/components/RewardsPopup.tsx
+++ src/components/RewardsPopup.tsx
@@ -140,7 +140,7 @@ export default function RewardsPopup({ isOpen, onClose, triggerRef }: RewardsPop
               <div className="w-80">
                 <div className="relative h-64 w-full">
                   {/* Chart Area */}
-                  <svg className="w-full h-full" viewBox="0 0 300 200">
+                  <svg className="w-full h-full" viewBox="0 0 240 200">
                     {/* Area fill under the line */}
                     <defs>
                       <linearGradient id="areaGradient" x1="0%" y1="0%" x2="0%" y2="100%">
@@ -151,13 +151,13 @@ export default function RewardsPopup({ isOpen, onClose, triggerRef }: RewardsPop
                     
                     {/* Area fill */}
                     <path
-                      d="M 30 150 Q 80 120 130 90 Q 180 60 220 40"
+                      d="M 30 150 Q 80 120 130 90 Q 180 60 210 40"
                       fill="url(#areaGradient)"
                     />
                     
                     {/* Line */}
                     <path
-                      d="M 30 150 Q 80 120 130 90 Q 180 60 220 40"
+                      d="M 30 150 Q 80 120 130 90 Q 180 60 210 40"
                       fill="none"
                       stroke={signatureColor}
                       strokeWidth="3"
